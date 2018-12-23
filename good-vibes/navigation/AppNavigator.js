2a6913d62111b2ae50b5348327205e5413be8be3
Rohit Kaundal <rohit@vassagoconsultancy.com>
2018-12-24 00:09:41 +0530

    added comments

--- good-vibes/navigation/AppNavigator.js
+++ good-vibes/navigation/AppNavigator.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createSwitchNavigator } from "react-navigation";
 
-import MainTabNavigator from './MainTabNavigator';
+//Import our bottom tab navigator
+import MainTabNavigator from "./MainTabNavigator";
 
+// Create switch navigator with our main bottom tag navigator and export it
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: MainTabNavigator
 });
